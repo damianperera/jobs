4c9e32b306b2e00fd5b854d5026f05ca651a1a47
Damian Perera <damianperera@outlook.com>
2022-11-19 20:28:27 +0100

    added code to read from sheets

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,24 @@
+import useGoogleSheets from 'use-google-sheets';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const { data, loading, error } = useGoogleSheets({
+    apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+    sheetId: process.env.REACT_APP_GOOGLE_SHEETS_ID,
+  });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error!</div>;
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {JSON.stringify(data)}
     </div>
   );
 }
