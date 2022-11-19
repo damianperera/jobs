3c53fce622d61de773f1af7f0d081ca8eab78f5f
Damian Perera <damianperera@outlook.com>
2022-11-19 21:00:44 +0100

    added effects

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,25 @@
+import * as React from 'react';
 import useGoogleSheets from 'use-google-sheets';
 import './App.css';
 
 const App = () => {
+  const [jobs, setJobs] = React.useState([]);
+
   const { data, loading, error } = useGoogleSheets({
     apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
     sheetId: process.env.REACT_APP_GOOGLE_SHEETS_ID,
   });
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error!</div>;
-  }
+  React.useEffect(() => {
+    const onlyJobs = data[0]
+    if (!loading && !error) {
+      setJobs(onlyJobs['data'].slice(3))
+    }
+  }, [ data, error, loading ])
 
   return (
     <div className="App">
-      {JSON.stringify(data)}
+      {JSON.stringify(jobs)}
     </div>
   );
 }
