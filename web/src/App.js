6264455ac391cc7d0eec39ef621c44b4dcb2f65d
Damian Perera <damianperera@outlook.com>
2022-11-20 06:17:39 +0100

    added sticky header

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,8 @@
 import * as React from 'react';
 import useGoogleSheets from 'use-google-sheets';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { KeyRounded, GitHub } from "@mui/icons-material"
+import { CssBaseline, Typography, Button, CardContent, CardActions, Card, Grid, AppBar, Toolbar, IconButton, Menu, Drawer, ListItemText, Divider, ListItem, ListItemIcon, ListItemButton, Tooltip, Box, Slide, useScrollTrigger } from '@mui/material';
 
 function StyledCard({ company, about, links, roles, locations, pros, comments, notes }) {
   return (
@@ -42,7 +38,7 @@ function StyledCard({ company, about, links, roles, locations, pros, comments, n
           sx={{
             overflow: "hidden",
             display: "-webkit-box",
-            WebkitLineClamp: 3,
+            WebkitLineClamp: 2,
             WebkitBoxOrient: "vertical"
           }}
         >
@@ -57,7 +53,15 @@ function StyledCard({ company, about, links, roles, locations, pros, comments, n
 }
 
 const App = () => {
-  const [jobs, setJobs] = React.useState([]);
+  const [jobs, setJobs] = React.useState([])
+  const [menuToggle, setMenuToggle] = React.useState(false)
+
+	const toggleMenu = (open) => (event) => {
+		if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
+			return
+		}
+		setMenuToggle(open)
+	}
 
   const { data, loading, error } = useGoogleSheets({
     apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
@@ -92,7 +96,76 @@ const App = () => {
 
   return (
     <ThemeProvider theme={darkTheme}>
-      <Grid container spacing={2} columns={{ xs: 3, sm: 6, md: 12 }} sx={{padding: "4%", backgroundColor: "black"}}>
+      <CssBaseline />
+      <AppBar position="sticky">
+			<Toolbar sx={{ height: "10vh" }}>
+				<IconButton
+					size="large"
+					edge="start"
+					color="inherit"
+					aria-label="menu"
+					sx={{ mr: 2 }}
+					onClick={toggleMenu(true)}
+				>
+					<Menu />
+				</IconButton>
+				{/* <Drawer open={menuToggle} onClose={setMenuToggle(false)}>
+          <Box
+            sx={{ width: 200 }}
+            role="presentation"
+            onClick={setMenuToggle(false)}
+            onKeyDown={setMenuToggle(false)}
+          >
+            <ListItemText
+              primary="Home"
+              secondary="Dev Homepage"
+              sx={{
+                marginLeft: 1,
+                ".MuiListItemText-primary": { fontWeight: "bold" },
+              }}
+            />
+            <Divider />
+            <Box
+              sx={{
+                position: "fixed",
+                bottom: 0,
+                width: 200,
+              }}
+            >
+              <Divider />
+              <Tooltip title="GitHub">
+                <ListItem key="repository" disablePadding>
+                  <ListItemButton 
+                    // onClick={handleRepositoryClick}
+                  >
+                    <ListItemIcon>
+                      <GitHub />
+                    </ListItemIcon>
+                    <ListItemText primary="Repository" />
+                  </ListItemButton>
+                </ListItem>
+              </Tooltip>
+              <Tooltip title="MIT License">
+                <ListItem key="license" disablePadding>
+                  <ListItemButton 
+                    // onClick={handleLicenseClick}
+                  >
+                    <ListItemIcon>
+                      <KeyRounded />
+                    </ListItemIcon>
+                    <ListItemText primary="License" />
+                  </ListItemButton>
+                </ListItem>
+              </Tooltip>
+            </Box>
+          </Box>
+        </Drawer> */}
+				<Box display="flex" justifyContent="center" alignItems="center" sx={{ flexGrow: 1 }}>
+					
+				</Box>
+			</Toolbar>
+		</AppBar>
+      <Grid container spacing={2} columns={{ xs: 3, sm: 6, md: 12 }} sx={{paddingLeft: "4%", paddingRight: "4%", paddingTop: "2%", paddingBottom: "2%", backgroundColor: "black"}}>
         {jobs.map( job => (
           <Grid key={job['key']} item xs={3}>
             <StyledCard {...job} />
