d56d9ef62b555892b0d15dea69fb1a0bcd03fa89
Damian Perera <damianperera@outlook.com>
2022-11-21 07:23:48 +0100

    added community notes

--- web/src/App.js
+++ web/src/App.js
@@ -2,52 +2,52 @@ import * as React from 'react'
 import useGoogleSheets from 'use-google-sheets'
 import extractUrls from 'extract-urls'
 import { debounce } from 'lodash'
-import { ThemeProvider, createTheme } from "@mui/material/styles"
-import { KeyRounded, GitHub, Menu, Place, Search, OpenInNew, Add, Lightbulb, ContentCopy } from "@mui/icons-material"
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { KeyRounded, GitHub, Menu, Place, Search, OpenInNew, Add, Lightbulb, ContentCopy } from '@mui/icons-material'
 import { CssBaseline, Typography, Button, CardContent, CardActions, Card, Grid, AppBar, Toolbar, IconButton, Drawer, ListItemText, Divider, ListItem, ListItemIcon, ListItemButton, Tooltip, Box, TextField, Container, CircularProgress, Stack } from '@mui/material'
 
 function StyledCard(props) {
   return (
     <Card sx={{
       height: '250px',
-      display: "flex",
-      flexDirection: "column"
+      display: 'flex',
+      flexDirection: 'column'
     }}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        <Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
           {/* { about } */}
         </Typography>
-        <Typography variant="h5" component="div">
+        <Typography variant='h5' component='div'>
           {props.company}
         </Typography>
         <Typography
-          variant="body2"
+          variant='body2'
           sx={{
-            overflow: "hidden",
-            display: "-webkit-box",
+            overflow: 'hidden',
+            display: '-webkit-box',
             WebkitLineClamp: 1,
-            WebkitBoxOrient: "vertical",
+            WebkitBoxOrient: 'vertical',
             mb: 1.5
           }}
-          color="text.secondary"
+          color='text.secondary'
         >
           <Place sx={{ fontSize: '1rem', verticalAlign: 'middle', paddingBottom: '1%' }} /> {props.locations}
         </Typography>
-        <Divider sx={{ marginTop: "2%", marginBottom: "3%" }} />
+        <Divider sx={{ marginTop: '2%', marginBottom: '3%' }} />
         <Typography
-          variant="body1"
+          variant='body1'
           sx={{
-            overflow: "hidden",
-            display: "-webkit-box",
+            overflow: 'hidden',
+            display: '-webkit-box',
             WebkitLineClamp: 4,
-            WebkitBoxOrient: "vertical",
+            WebkitBoxOrient: 'vertical',
           }}
         >
           {props.roles}
         </Typography>
       </CardContent>
-      <CardActions sx={{ marginTop: "auto" }}>
-        <Button size="small" onClick={() => {
+      <CardActions sx={{ marginTop: 'auto' }}>
+        <Button size='small' onClick={() => {
           props.setSelectedJob(props)
         }}>Learn More</Button>
       </CardActions>
@@ -58,52 +58,69 @@ function StyledCard(props) {
 const ViewMoreSlider = (props) => {
   const showSlider = props.selectedJob !== null
   return (
-    <Drawer open={showSlider} anchor="right" onClose={() => { props.setSelectedJob(null) }} >
+    <Drawer open={showSlider} anchor='right' onClose={() => { props.setSelectedJob(null) }} >
       <Box
-        sx={{ width: 600, maxWidth: "80vw" }}
-        role="presentation"
+        sx={{ width: 600, maxWidth: '80vw' }}
+        role='presentation'
       >
         {showSlider &&
-          <Box sx={{ "& .MuiListItemText-root": { paddingTop: "1%", paddingLeft: "1%", paddingBottom: "2%", paddingRight: "1%" } }}>
+          <Box sx={{ '& .MuiListItemText-root': { paddingTop: '1%', paddingLeft: '1%', paddingBottom: '2%', paddingRight: '1%' } }}>
             <ListItemText
               primary={props.selectedJob.company}
               secondary={props.selectedJob.about}
               sx={{
                 marginLeft: 1,
-                ".MuiListItemText-primary": { fontWeight: "bold", fontSize: 30 },
+                '.MuiListItemText-primary': { fontWeight: 'bold', fontSize: 30 },
               }}
             />
             <Divider />
             <ListItemText
-              primary="Locations"
-              secondary={props.selectedJob.locations ?? "N/A"}
+              primary='Locations'
+              secondary={props.selectedJob.locations ?? '-'}
               sx={{
                 marginLeft: 1,
-                ".MuiListItemText-primary": { fontWeight: "bold", fontSize: 14 },
+                '.MuiListItemText-primary': { fontWeight: 'bold', fontSize: 14 },
               }}
             />
             <ListItemText
-              primary="Vacancies"
-              secondary={props.selectedJob.roles ?? "N/A"}
+              primary='Vacancies'
+              secondary={props.selectedJob.roles ?? '-'}
               sx={{
                 marginLeft: 1,
-                ".MuiListItemText-primary": { fontWeight: "bold", fontSize: 14 },
+                '.MuiListItemText-primary': { fontWeight: 'bold', fontSize: 14 },
+              }}
+            />
+            <Divider />
+            <ListItemText
+              primary='From the Community'
+              sx={{
+                marginLeft: 1,
+                marginBottom: -2,
+                '.MuiListItemText-primary': { fontWeight: 'bold', fontSize: 18 },
               }}
             />
             <ListItemText
-              primary="Community Review"
-              secondary={props.selectedJob.pros ?? "N/A"}
+              primary='Positives'
+              secondary={props.selectedJob.pros ?? '-'}
               sx={{
                 marginLeft: 1,
-                ".MuiListItemText-primary": { fontWeight: "bold", fontSize: 14 },
+                '.MuiListItemText-primary': { fontWeight: 'bold', fontSize: 14 },
               }}
             />
             <ListItemText
-              primary="Notes from Gergely Orosz"
-              secondary={props.selectedJob.notes ?? "N/A"}
+              primary='Comments'
+              secondary={props.selectedJob.comments ?? '-'}
               sx={{
                 marginLeft: 1,
-                ".MuiListItemText-primary": { fontWeight: "bold", fontSize: 14 },
+                '.MuiListItemText-primary': { fontWeight: 'bold', fontSize: 14 },
+              }}
+            />
+            <ListItemText
+              primary='Notes from the Curator'
+              secondary={props.selectedJob.notes ? `${props.selectedJob.notes} ~ Gergely Orosz` : '-'}
+              sx={{
+                marginLeft: 1,
+                '.MuiListItemText-primary': { fontWeight: 'bold', fontSize: 14 },
               }}
             />
             <Divider />
@@ -112,13 +129,13 @@ const ViewMoreSlider = (props) => {
                 <ListItem key={idx} disablePadding>
                   <ListItemButton
                     onClick={() => {
-                      window.open(link, "_blank", "noopener,noreferrer")
+                      window.open(link, '_blank', 'noopener,noreferrer')
                     }}
                   >
                     <ListItemIcon>
                       <OpenInNew />
                     </ListItemIcon>
-                    <ListItemText primary={`View Vacancies ${props.selectedJob.links.length === 1 ? '' : idx + 1}`} sx={{ marginLeft: "-3%" }} />
+                    <ListItemText primary={`View Vacancies ${props.selectedJob.links.length === 1 ? '' : idx + 1}`} sx={{ marginLeft: '-3%' }} />
                   </ListItemButton>
                 </ListItem>
               ))
@@ -133,7 +150,7 @@ const ViewMoreSlider = (props) => {
                 <ListItemIcon>
                   <ContentCopy />
                 </ListItemIcon>
-                <ListItemText primary={`Copy Link`} sx={{ marginLeft: "-3%" }} />
+                <ListItemText primary={`Copy Link`} sx={{ marginLeft: '-3%' }} />
               </ListItemButton>
             </ListItem>
           </Box>
@@ -151,7 +168,7 @@ const App = () => {
   const [selectedJob, setSelectedJob] = React.useState(null)
 
   const toggleMenu = (open) => (event) => {
-    if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
       return
     }
     setMenuToggle(open)
@@ -160,17 +177,17 @@ const App = () => {
   const { data, loading, error } = useGoogleSheets({
     apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
     sheetId: process.env.REACT_APP_GOOGLE_SHEETS_ID,
-  });
+  })
 
   const darkTheme = createTheme({
     palette: {
-      mode: "dark",
+      mode: 'dark',
     },
   })
 
   React.useEffect(() => {
     const queryParams = new URLSearchParams(window.location.search)
-    const requestedCompanyId = queryParams.get("company")
+    const requestedCompanyId = queryParams.get('company')
 
     if (loading) {
       setIsLoading(true)
@@ -206,13 +223,13 @@ const App = () => {
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <ViewMoreSlider selectedJob={selectedJob} setSelectedJob={setSelectedJob} />
-      <AppBar position="sticky">
-        <Toolbar sx={{ height: "10vh" }}>
+      <AppBar position='sticky'>
+        <Toolbar sx={{ height: '10vh' }}>
           <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
+            size='large'
+            edge='start'
+            color='inherit'
+            aria-label='menu'
             sx={{ mr: 2 }}
             onClick={toggleMenu(true)}
           >
@@ -221,90 +238,90 @@ const App = () => {
           <Drawer open={menuToggle} onClose={toggleMenu(false)}>
             <Box
               sx={{ width: 200 }}
-              role="presentation"
+              role='presentation'
               onClick={toggleMenu(false)}
               onKeyDown={toggleMenu(false)}
             >
               <ListItemText
-                primary="Tech Jobs"
-                secondary="Community Sourced"
+                primary='Tech Jobs'
+                secondary='Community Sourced'
                 sx={{
                   marginLeft: 1,
-                  ".MuiListItemText-primary": { fontWeight: "bold" },
+                  '.MuiListItemText-primary': { fontWeight: 'bold' },
                 }}
               />
               <Divider />
-              <ListItem key="create-new" disablePadding>
+              <ListItem key='create-new' disablePadding>
                 <ListItemButton
                   onClick={() => {
-                    window.open("https://docs.google.com/forms/d/e/1FAIpQLSfSCZGnIwt_yDIPdGq6o_uhj2WQjIPrU_wa0Hb4P3E6D8M3jw/viewform", "_blank", "noopener,noreferrer")
+                    window.open('https://docs.google.com/forms/d/e/1FAIpQLSfSCZGnIwt_yDIPdGq6o_uhj2WQjIPrU_wa0Hb4P3E6D8M3jw/viewform', '_blank', 'noopener,noreferrer')
                   }}
                 >
                   <ListItemIcon>
                     <Add />
                   </ListItemIcon>
-                  <ListItemText primary="Add a Company" sx={{ marginLeft: "-10%" }} />
+                  <ListItemText primary='Add a Company' sx={{ marginLeft: '-10%' }} />
                 </ListItemButton>
               </ListItem>
-              <ListItem key="google-sheets-source" disablePadding>
+              <ListItem key='google-sheets-source' disablePadding>
                 <ListItemButton
                   onClick={() => {
-                    window.open("https://docs.google.com/spreadsheets/d/1SMKjAgYxG1iAi_G4E3DJik17-EkO8QiTo6obeZCiBAQ/htmlview?usp=sharing&pru=AAABhLTG1qE*m-nkG9q9MVwWQN9sQQ9DbQ#", "_blank", "noopener,noreferrer")
+                    window.open('https://docs.google.com/spreadsheets/d/1SMKjAgYxG1iAi_G4E3DJik17-EkO8QiTo6obeZCiBAQ/htmlview?usp=sharing&pru=AAABhLTG1qE*m-nkG9q9MVwWQN9sQQ9DbQ#', '_blank', 'noopener,noreferrer')
                   }}
                 >
                   <ListItemIcon>
                     <Lightbulb />
                   </ListItemIcon>
-                  <ListItemText primary="Learn More" sx={{ marginLeft: "-10%" }} />
+                  <ListItemText primary='Learn More' sx={{ marginLeft: '-10%' }} />
                 </ListItemButton>
               </ListItem>
               <Box
                 sx={{
-                  position: "fixed",
+                  position: 'fixed',
                   bottom: 0,
                   width: 200,
                 }}
               >
                 <Divider />
-                <Tooltip title="GitHub">
-                  <ListItem key="repository" disablePadding>
+                <Tooltip title='GitHub'>
+                  <ListItem key='repository' disablePadding>
                     <ListItemButton
                       onClick={() => {
-                        window.open("https://github.com/damianperera/jobs", "_blank", "noopener,noreferrer")
+                        window.open('https://github.com/damianperera/jobs', '_blank', 'noopener,noreferrer')
                       }}
                     >
                       <ListItemIcon>
                         <GitHub />
                       </ListItemIcon>
-                      <ListItemText primary="Repository" sx={{ marginLeft: "-10%" }} />
+                      <ListItemText primary='Repository' sx={{ marginLeft: '-10%' }} />
                     </ListItemButton>
                   </ListItem>
                 </Tooltip>
-                <Tooltip title="MIT License">
-                  <ListItem key="license" disablePadding>
+                <Tooltip title='MIT License'>
+                  <ListItem key='license' disablePadding>
                     <ListItemButton
                       onClick={() => {
-                        window.open("https://github.com/damianperera/jobs/blob/main/LICENSE", "_blank", "noopener,noreferrer")
+                        window.open('https://github.com/damianperera/jobs/blob/main/LICENSE', '_blank', 'noopener,noreferrer')
                       }}
                     >
                       <ListItemIcon>
                         <KeyRounded />
                       </ListItemIcon>
-                      <ListItemText primary="License" sx={{ marginLeft: "-10%" }} />
+                      <ListItemText primary='License' sx={{ marginLeft: '-10%' }} />
                     </ListItemButton>
                   </ListItem>
                 </Tooltip>
               </Box>
             </Box>
           </Drawer>
-          <Box display="flex" justifyContent="center" alignItems="center" sx={{ flexGrow: 1 }}>
-            <Container maxWidth="sm">
+          <Box display='flex' justifyContent='center' alignItems='center' sx={{ flexGrow: 1 }}>
+            <Container maxWidth='sm'>
               <TextField
                 autoFocus
                 required
-                id="search"
+                id='search'
                 placeholder={`Search from ${jobs.length} community sourced jobs in tech`}
-                variant="outlined"
+                variant='outlined'
                 onChange={debounce((event) => {
                   const text = event.target.value
                   if (text) {
@@ -315,23 +332,23 @@ const App = () => {
                 }, 250)
                 }
                 sx={{
-                  border: "1px solid rgba(81, 81, 81, 1)",
-                  borderRadius: "4px",
-                  width: "100%",
-                  "& .MuiOutlinedInput-root.Mui-focused": {
-                    "& > fieldset": {
+                  border: '1px solid rgba(81, 81, 81, 1)',
+                  borderRadius: '4px',
+                  width: '100%',
+                  '& .MuiOutlinedInput-root.Mui-focused': {
+                    '& > fieldset': {
                       border: 0,
                     },
                   },
-                  "& .MuiInputBase-root": {
-                    "& > fieldset": {
-                      border: "0 !important",
+                  '& .MuiInputBase-root': {
+                    '& > fieldset': {
+                      border: '0 !important',
                     },
                   },
                 }}
                 InputProps={{
                   endAdornment: (
-                    <IconButton aria-label="search" size="small">
+                    <IconButton aria-label='search' size='small'>
                       <Search />
                     </IconButton>
                   ),
@@ -345,18 +362,18 @@ const App = () => {
         <Grid
           container
           spacing={0}
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
+          direction='column'
+          alignItems='center'
+          justifyContent='center'
           style={{ minHeight: '100vh' }}
         >
           <Grid item xs={3}>
             <Stack
-              direction="row"
-              alignItems="center"
-              justifyContent="center"
+              direction='row'
+              alignItems='center'
+              justifyContent='center'
               gap={1}
-              sx={{ paddingTop: "2%" }}
+              sx={{ paddingTop: '2%' }}
             >
               <CircularProgress size={25} /> Loading
             </Stack>
@@ -364,8 +381,8 @@ const App = () => {
           </Grid>
         </Grid>
       )}
-      <Box sx={{ height: "100%", width: "100%", color: "black", p: 3 }}>
-        <Grid display="flex" container spacing={3} columns={{ xs: 3, sm: 6, md: 12 }}>
+      <Box sx={{ height: '100%', width: '100%', color: 'black', p: 3 }}>
+        <Grid display='flex' container spacing={3} columns={{ xs: 3, sm: 6, md: 12 }}>
           {searchedJobs.map(job => (
             <Grid key={job['key']} item xs={3}>
               <StyledCard {...job} setSelectedJob={setSelectedJob} />
@@ -374,7 +391,7 @@ const App = () => {
         </Grid>
       </Box>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
