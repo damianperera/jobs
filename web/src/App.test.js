2abfa9084e3fa640b93c76e7b95827c03e103418
Damian Perera <damianperera@outlook.com>
2023-01-11 05:43:05 +0100

    fixed test

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders search bar', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const searchBar = screen.getByPlaceholderText(/Search from 0 community sourced jobs in tech/i);
+  expect(searchBar).toBeInTheDocument();
 });
